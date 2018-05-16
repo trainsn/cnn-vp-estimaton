205ccf94074ae04df4beb1fc3c94e9660f8c6181
cad <shaoxinxiaosai@163.com>
2018-05-16 13:14:01 +0800

    change simi metric param from 9 to 5

--- train_command.sh
+++ train_command.sh
@@ -23,9 +23,8 @@ nohup ./build/tools/caffe test --model=/home/cad/disk/linux/RenderForCNN-master/
 nohup ./tools/caffe test --model=/home/cad/disk/linux/RenderForCNN-master/train/train_val_angle.prototxt -weights snapshots_iter_9600.caffemodel -gpu 0 -iterations 120 > output_engine_test.log 2>&1 &
 
 
-nohup ./build/examples/cpp_classification/classification.bin   /home/cad/disk/linux/RenderForCNN-master/train/deploy1.prototxt  /home/cad/disk/linux/RenderForCNN-master/caffe_models/model-tooth/snapshots_tooth.caffemodel  /home/cad/disk/linux/RenderForCNN-master/train/imagenet_mean.binaryproto   /home/cad/disk/linux/RenderForCNN-master/train/angle_class.txt  /home/cad/disk/linux/RenderForCNN-master/data/tooth/img-pca/all_cla.txt > cla_idx.txt 2>&1 1&
+nohup ./build/examples/cpp_classification/classification.bin   /home/cad/disk/linux/RenderForCNN-master/train/deploy1.prototxt  /home/cad/disk/linux/RenderForCNN-master/caffe_models/model-engine/snapshots_engine.caffemodel  /home/cad/disk/linux/RenderForCNN-master/train/imagenet_mean.binaryproto   /home/cad/disk/linux/RenderForCNN-master/train/angle_class.txt  /home/cad/disk/linux/RenderForCNN-master/data/engine/img-pca/all_cla.txt > cla_idx.txt 2>&1 1&
 
-#
 nohup ./build/examples/cpp_classification/classification.bin   /home/cad/disk/linux/RenderForCNN-master/train/classify/deploy_view.prototxt   model-classify/VIEW_CLASSIFY_iter_600.caffemodel   /home/cad/disk/linux/RenderForCNN-master/train/imagenet_mean.binaryproto   /home/cad/disk/linux/RenderForCNN-master/train/classify/class.txt   /media/cad/3fafa74e-c460-4512-8fb2-4a08ea3c1ef7/classify/all_cla.txt 2 > cla_res.txt 2>&1 &
 
 #/home/cad/caffe-master

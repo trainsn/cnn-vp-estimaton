5cb2c96a6d2631046a351f43b6b82a6bc926364d
cad <shaoxinxiaosai@163.com>
2018-06-11 19:01:06 +0800

    add vggnet

--- train_command.sh
+++ train_command.sh
@@ -3,8 +3,8 @@ nohup ./build/tools/caffe train --solver=/home/cad/disk/linux/RenderForCNN-maste
 #caffe-render-for-cnn-view_prediction
 nohup ./tools/caffe train --solver=/home/cad/disk/linux/RenderForCNN-master/train/solver_angle.prototxt -weights /home/cad/disk/linux/RenderForCNN-master/caffe_models/render4cnn_3dview.caffemodel > output.log 2>&1 &
 
-nohup ./build/tools/caffe train --solver=/home/shineng/vp_train/solver_server_tree1.prototxt -weights /home/shineng/vp_train/render4cnn_3dview.caffemodel --gpu 1 > output_tree1.log 2>&1 &
-nohup ./build/tools/caffe train --solver=/home/shineng/vp_train/solver_server_tree.prototxt -snapshot  snapshots_tree_iter_32000.solverstate --gpu 3 > output_tree.log 2>&1 &
+nohup ./build/tools/caffe train --solver=/home/shineng/vp_train/solver_server_fish.prototxt -weights /home/shineng/vp_train/render4cnn_3dview.caffemodel --gpu 0 > output_fish.log 2>&1 &
+nohup ./build/tools/caffe train --solver=/home/shineng/vp_train/solver_server_engine.prototxt -snapshot  snapshots_engine_iter_800.solverstate --gpu 1 > output_engine.log 2>&1 &
 
 nohup ./build/tools/caffe train --solver=/home/cad/disk/linux/RenderForCNN-master/train/solver.prototxt -snapshot  snapshots_iter_13200.solverstate --gpu 1 > output.log 2>&1 &
 
@@ -13,14 +13,16 @@ nohup ./build/tools/caffe train --solver=/home/cad/disk/linux/RenderForCNN-maste
 
 nohup ./build/tools/caffe test --model=//home/cad/disk/linux/RenderForCNN-master/train/classify/view_classify.prototxt -weights VIEW_CLASSIFY_iter_600.caffemodel -gpu 0 -iterations 300 > output_classify_test.log 2>&1 &g
 
-vggnet微调
-nohup ./build/tools/caffe train --solver=/home/cad/disk/linux/RenderForCNN-master/train/classify/solver_vgg.prototxt -weights /home/cad/disk/linux/RenderForCNN-master/caffe_models/VGG_ILSVRC_16_layers.caffemodel > output_cla.txt 2>&1 &
+#vggnet微调
+nohup ./build/tools/caffe train --solver=/home/cad/disk/linux/RenderForCNN-master/train/vggnet/solver.prototxt -weights /home/cad/disk/linux/RenderForCNN-master/caffe_models/VGG_ILSVRC_16_layers.caffemodel >   output.log 2>&1 &
 
-测试
-nohup ./build/tools/caffe test --model=/home/cad/disk/linux/RenderForCNN-master/train/train_val_sphere1.prototxt -weights model-tree/snapshots_tree.caffemodel -gpu 0 -iterations 20 > output_tree_test.log 2>&1 &
+nohup ./build/tools/caffe train --solver=/home/shineng/vp_train/vggnet/solver.prototxt -weights /home/shineng/vp_train/vggnet/VGG_ILSVRC_16_layers.caffemodel --gpu 0  > output.log 2>&1 &
+
+#测试
+nohup ./build/tools/caffe test --model=/home/cad/disk/linux/RenderForCNN-master/train/train_val_sphere1.prototxt -weights snapshots_engine.caffemodel -gpu 0 -iterations 60 > output_engine_test.log 2>&1 &
 
 #caffe-render-for-cnn-view_prediction
-nohup ./tools/caffe test --model=/home/cad/disk/linux/RenderForCNN-master/train/train_val_angle.prototxt -weights snapshots_iter_9600.caffemodel -gpu 0 -iterations 120 > output_tree_test.log 2>&1 &
+nohup ./tools/caffe test --model=/home/cad/disk/linux/RenderForCNN-master/train/train_val_angle.prototxt -weights snapshots_iter_9400.caffemodel -gpu 0 -iterations 120 > output_head_test.log 2>&1 &
 
 
 nohup ./build/examples/cpp_classification/classification.bin   /home/cad/disk/linux/RenderForCNN-master/train/deploy1.prototxt  /home/cad/disk/linux/RenderForCNN-master/caffe_models/model-tree/snapshots_tree.caffemodel  /home/cad/disk/linux/RenderForCNN-master/train/imagenet_mean.binaryproto   /home/cad/disk/linux/RenderForCNN-master/train/angle_class.txt  /home/cad/disk/linux/RenderForCNN-master/data/tree/img-pca/all_cla.txt > cla_idx.txt 2>&1 1&
